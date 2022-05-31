661f58fd0da6af1b38e1235cbbcf87dfa7122fef
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-05-31 13:49:04 -0600

    check bounds, support empty matrix

--- src/gather.jl
+++ src/gather.jl
@@ -26,10 +26,21 @@ function gather_check_dims(Nx, Ny, M, Nidx)
     return dims
 end
 
+function gather_checkbounds(X::AbstractArray{Tx,Nx},
+                            idx::AbstractArray,
+                            dims::Int) where
+                            {Tx,Nx}
+    max_idx_j = size(X)[dims+1:end]
+    idx_i = CartesianIndices(size(X)[1:dims])
+    for idx_j in idx
+        all(idx_j.<= max_idx_j) || X[idx_i,idx_j]
+    end    
+end
+
 function gather_kernel!(dst, src, idx, max_idx, max_dims_idx, dims_size)
     index = threadIdx().x + (blockIdx().x - 1) * blockDim().x
 
-    @inbounds if index <= max_idx
+    if index <= max_idx
         j, k = divrem(index-1, max_dims_idx)
         dims_i = CartesianIndices(dims_size)[k+1]
         dst[index] = src[dims_i, idx[j+1]...]
@@ -40,7 +51,7 @@ end
 function gather_kernel!(dst, src, idx::CUDA.CuDeviceArray{<:CartesianIndex}, max_idx, max_dims_idx, dims_size)
     index = threadIdx().x + (blockIdx().x - 1) * blockDim().x
 
-    @inbounds if index <= max_idx
+    if index <= max_idx
         j, k = divrem(index-1, max_dims_idx)
         dims_i = CartesianIndices(dims_size)[k+1]
         li = Base._to_linear_index(src, Tuple(dims_i)..., Tuple(idx[j+1])...)
@@ -51,6 +62,10 @@ end
 
 function NNlib.gather!(dst::AnyCuArray, src::AnyCuArray, idx::AnyCuArray)
     dims = gather_check_dims(src, dst, idx)
+    if isempty(src)
+        gather_checkbounds(src, idx, dims)
+        return dst
+    end
     dims_size = size(src)[1:dims]
     max_dims_idx = prod(dims_size)
     max_idx = max_dims_idx * length(idx)
